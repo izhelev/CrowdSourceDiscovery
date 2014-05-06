636248399a329cb2997d0743bbe127b083795e3c
izhelev <ivailo.zhelev@gmail.com>
2014-05-06 23:24:43 +0100

    Installed boostrap mvc5 project

---
+++ CrowdSourceDiscovery.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrowdSourceDiscovery.Web.MvcApplication" Language="C#" %>
---
+++ CrowdSourceDiscovery.Web/Startup.cs
@@ -0,0 +1,15 @@
+﻿using CrowdSourceDiscovery.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace CrowdSourceDiscovery.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
