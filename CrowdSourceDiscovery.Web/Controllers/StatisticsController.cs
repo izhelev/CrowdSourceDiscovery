88f35b5c15227dcac4dc354fc9f1e0af4b3946ef
izhelev <ivailo.zhelev@gmail.com>
2014-05-25 23:41:32 +0100

    Small changes to UI.

---
+++ CrowdSourceDiscovery.Web/Controllers/StatisticsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Mvc;
+using CrowdSourceDiscovery.Web.Models;
+using Microsoft.AspNet.Identity;
+
+namespace CrowdSourceDiscovery.Web.Controllers
+{
+    public class StatisticsController : Controller
+    {
+       [ChildActionOnly]
+        public ActionResult ExternalMonitoring()
+       {
+           if (!User.Identity.IsAuthenticated)
+               return View(new ExternalMonitoringModel());
+
+           var statisticsModel = new ExternalMonitoringModel()
+           {
+               UserId = new Guid(User.Identity.GetUserId()),
+               Username = User.Identity.GetUserName()
+           };
+
+           return View(statisticsModel);
+        }
+	}
+}
---
+++ CrowdSourceDiscovery.Web/Models/ExternalMonitoringModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CrowdSourceDiscovery.Web.Models
+{
+    public class ExternalMonitoringModel
+    {
+        public ExternalMonitoringModel()
+        {
+            UserId = new Guid();
+            Username = "Anonymous";
+        }
+
+        public Guid UserId { get; set; }
+        public string Username { get; set; }
+    }
+}
